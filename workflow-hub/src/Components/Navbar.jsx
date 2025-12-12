c15ed6fcf98348c11b0e01100d87b9ed5c81d522
misbahighizlan-ship-it <misbahighizlan@gmail.com>
2025-12-12 11:47:09 +0000

    change css and form

--- workflow-hub/src/Components/Navbar.jsx
+++ workflow-hub/src/Components/Navbar.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import { FaHome, FaTrash } from 'react-icons/fa'
+
 export default function Navbar() {
     return (
         <nav className="Navbar">
-            <div >
-                 <img className='logo' src="/photo/logo.png" alt="logo" />
-            </div>
+
             <div className='Nav-title'>
                 <h1>WorkflowHub Lite</h1>
             </div>
 
             <div className='Nav-links'>
-                <Link to="/" > Board</Link>
-                <Link to="/corbeille"> Trash</Link>
+                <Link to="/"><FaHome /> Board</Link>
+                <Link to="/corbeille"><FaTrash /> Trash</Link>
             </div>
-
-
-        </nav>
+        </nav>   
     )
 }
